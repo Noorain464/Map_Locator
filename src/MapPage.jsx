356c9f9260a5a2a4146c4226060a2ac20e2e5d4c
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-11-25 19:49:29 +0530

    commit

--- src/MapPage.jsx
+++ src/MapPage.jsx
@@ -19,16 +19,6 @@ function MapPage() {
       return 'Unknown Place'
     }
   }
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setNewPhoto(reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
   
   const [map, setMap] = useState(null)
   const [positions, setPositions] = useState(() => {
@@ -106,13 +96,13 @@ function MapPage() {
           <input
             type="file"
             accept="image/*"
-            onChange={handleImageChange}
+            onChange={e => setNewPhoto(e.target.files[0])}
           />
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <button
               onClick={() => {
                 setPositions([...positions, newPosition])
-                setDescriptions([...descriptions, { title: newTitle, message: newMessage, photo: newPhoto || null }])
+                setDescriptions([...descriptions, { title: newTitle, message: newMessage, photo: newPhoto ? URL.createObjectURL(newPhoto) : null }])
                 setNewPosition(null)
               }}
               className='save'
